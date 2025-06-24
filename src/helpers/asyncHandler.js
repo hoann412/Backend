cb0ae9d3039cef7322e0fa23b357e3ec4bd6ca78
CaoChiPhat <122960893+CaoChiPhat2004@users.noreply.github.com>
2025-06-24 21:45:17 +0700

    Add files via upload
    khởi tạo base , kết nối db , Hằng số, xử lý lỗi , tiện ích chung và xác thực phân quyền

---
+++ src/helpers/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next);
+        } catch (error) {
+            next(error);
+        } finally {
+            if (req.session) {
+                req.session.endSession();
+            }
+        }
+    };
+};
+
+export default asyncHandler;
